b377331c88a0f5e058fd3005cf247e94d33f8a34
AleAC1 <51467367+AleAC1@users.noreply.github.com>
2022-12-16 04:41:26 -0500

    Update conexionBD.php

--- mvc/Modelos/conexionBD.php
+++ mvc/Modelos/conexionBD.php
@@ -1,7 +1,7 @@
 <?php 
 		class ConexionBD{
 			public static function cBD($usuario,$clave){
-				$bd = new PDO("mysql:host=dbprjcomputacion.cuevafgehoh7.us-east-1.rds.amazonaws.com;dbname=biblioteca",$usuario,$clave); 
+				$bd = new PDO("mysql:host=dbprjcomputacion.cuevafgehoh7.us-east-1.rds.amazonaws.com;port=3306;dbname=biblioteca",$usuario,$clave); 
 				return $bd;
 			}
 		}
